96e02dacc00b30cae68b43dbe6d42ca9657cf834
Edin <yamamoto.nakamoto@gmail.com>
2022-05-01 20:48:13 +0200

    Added CharInFilter for filtering REST endpoints with a comma-separated query param values

--- rateotu/orders/filters.py
+++ rateotu/orders/filters.py
@@ -2,13 +2,18 @@ from django_filters import rest_framework as filters
 from rateotu.orders.models import OrderItem
 
 
+class CharInFilter(filters.BaseInFilter, filters.CharFilter):
+    pass
+
+
 class OrderItemFilter(filters.FilterSet):
     item_category = filters.CharFilter(
         field_name="item__category__name",
         lookup_expr="exact",
         distinct=True,
     )
+    order_status = CharInFilter(field_name="order_status", lookup_expr="in")
 
     class Meta:
         model = OrderItem
-        fields = ["item_category"]
+        fields = ["item_category", "order_status"]
