5d00e0e596164051e93038312f96d7181369040c
Edin <yamamoto.nakamoto@gmail.com>
2022-05-01 15:40:48 +0200

    Added bulk_update_order_items_order_status service function

--- rateotu/orders/services.py
+++ rateotu/orders/services.py
@@ -14,17 +14,19 @@ from django.db import transaction
 from rateotu.orders.models import Order, OrderItem
 
 
+# Method decorated with @transaction.atomic to ensure
+# logic is executed in a single transaction
 @transaction.atomic
 def create_customer_order(customer, total, order_items):
     """
     Creates the order for the given customer instance
     and creates all order items linked to the order.
     """
-    # NOTE: Sets a customer instance based on a request user (check views)
+    # Sets a customer instance based on a request user (check views)
     # that will be provided after authenticating trough JWT auth.
     # We cannot trust a client to provide this piece of data!
     order = Order.objects.create(total=total, customer=customer)
-    # NOTE: Bulk saving (1 INSERT query), due to performance and speed.
+    # Bulk INSERT (1 query), due to performance and speed.
     OrderItem.objects.bulk_create(
         [
             OrderItem(
@@ -37,3 +39,23 @@ def create_customer_order(customer, total, order_items):
             for item in order_items
         ]
     )
+
+
+@transaction.atomic
+def bulk_update_order_items_order_status(order_item_ids, order_status):
+    """
+    Bulk update 'order_status' on all OrderItem instances for a given
+    'order_item_ids' iterable.
+    """
+    # Row-level locks to prevent updates with race conditions.
+    # This function can be used anywhere in the system (request-response cycle,
+    # Celery tasks, external scripts, etc).
+    # Therefore when a database operation is in progress, the object or the qs
+    # of objects that are being updated must be locked until the operation is complete,
+    # so that no other process (or thread) can access this qs objects.
+    order_items = OrderItem.objects.select_for_update().filter(id__in=order_item_ids)
+    # Loop over each item and invoke save() on instance.
+    for order_item in order_items:
+        order_item.order_status = order_status
+        # save() method called on each intance to update (signals are triggerd as well).
+        order_item.save()
