dc15699288a0ba19153d9d9da02671682660d96a
Edin <yamamoto.nakamoto@gmail.com>
2022-05-02 23:51:12 +0200

    Added async ORM selectors

---
+++ rateotu/utils/asyncio/selectors.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import get_user_model
+
+from channels.db import database_sync_to_async
+
+
+User = get_user_model()
+
+
+# This decorator turns ORM query from sync to async
+# and cleans up database connections on exit.
+@database_sync_to_async
+def get_user(field_name, value):
+    """
+    Tries to fetch a User obj for a given field_name=value.
+    """
+    return User.objects.get(**{field_name: value})
+
+
+@database_sync_to_async
+def get_employee_role(user):
+    """
+    Tries to fetch employees role for a given user.
+    """
+    return user.employee.role
