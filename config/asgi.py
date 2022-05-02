e3381cede8a0eaa353da50fadad7b153738a21d3
Edin <yamamoto.nakamoto@gmail.com>
2022-05-02 23:37:46 +0200

    Added EmployeeNotificationConsumer

--- config/asgi.py
+++ config/asgi.py
@@ -1,16 +1,21 @@
-"""
-ASGI config for rateotu project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
-"""
-
 import os
 
 from django.core.asgi import get_asgi_application
 
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+
+from rateotu.employees.routing import websocket_urlpatterns
+from rateotu.utils.websockets.auth import JWTAuthMiddleware
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 
-application = get_asgi_application()
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AllowedHostsOriginValidator(
+            JWTAuthMiddleware(URLRouter(websocket_urlpatterns))
+        ),
+    }
+)
---
+++ rateotu/employees/consumers.py
@@ -0,0 +1,44 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+
+from rateotu.utils.websockets.mixins import AsyncJsonEncoderDecoderMixin
+from rateotu.utils.websockets.selectors import get_employee_role
+
+
+class EmployeeNotificationConsumer(
+    AsyncJsonEncoderDecoderMixin, AsyncWebsocketConsumer
+):
+    """
+    Receives and broadcasts real-time data to all connected employees (WS clients).
+    """
+
+    async def connect(self):
+        self.user = self.scope["user"]
+        self.group_name = None
+
+        # Check if a user is authenticated trough JWT (SEE: utils/websocket/auth)
+        if not self.user.is_authenticated:
+            return await self.close()
+        # Check permissions
+        if not self.user.is_employee:
+            return await self.close()
+
+        # Crate the group name
+        self.group_name = f"ws_employee_{await get_employee_role(self.user)}s"
+        # Add channel to the group
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
+        # Accept connection as the last action in connect()
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        if self.group_name is not None:
+            await self.channel_layer.group_discard(self.group_name, self.channel_name)
+
+    # Receive a message from the group and broadcast to all connected channels
+    # (channels = different employees => one or many client pages, browser tabs,
+    # browser windows; client devices, etc).
+    async def broadcast_to_employees(self, event):
+        """
+        Called internally to send a message to a connected employee.
+        """
+        # Send JSON encoded message to employee
+        await self.send_json(event)
